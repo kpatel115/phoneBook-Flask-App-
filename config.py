3879909ca083cbd4d413f088ff6950bd5c731099
kpatel115 <kpatel9338448@gmail.com>
2023-10-16 10:02:25 -0600

    final commit - tested in insomnia and works

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
@@ -13,7 +11,7 @@ class Config():
     '''
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
-    SECRET_KEY = os.environ.get('SECRET_DAY') or 'passwordkey'
-    SQLALCHEMY_DB_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_NOTIFCATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'passwordkey'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_NOTIFICAITONS = False
     
